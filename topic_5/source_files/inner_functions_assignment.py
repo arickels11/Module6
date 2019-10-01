fd75e3f070c42cdbf25cb38084d497f8d3052b1c
arickels11 <arickels93@gmail.com>
2019-09-30 21:35:26 -0500

    Topic 4 Rectangle Commit

--- topic_5/source_files/inner_functions_assignment.py
+++ topic_5/source_files/inner_functions_assignment.py
@@ -3,14 +3,26 @@ Alex Rickels
 Module 6 Topic 5 Assignment"""
 
 
-def measurements(rec_list):
-    def area():
-        return float(rec_list[0] * rec_list[1])
+def measurements(rec_list):  # outer function
+    """
+    :param rec_list: variable list of lenth and width of rectangle, or length of a square
+    :return: area and perimeter of the rectangle
+    """
 
-    def perimeter():
-        return float((rec_list[0] * 2) + (rec_list[1] * 2))
+    def area():  # inner function
+        if len(rec_list) == 2:
+            return float(rec_list[0] * rec_list[1])
+        elif len(rec_list) == 1:
+            return float(rec_list[0] * rec_list[0])
+
+    def perimeter():  # inner function
+        if len(rec_list) == 2:
+            return float((rec_list[0] * 2) + (rec_list[1] * 2))
+        elif len(rec_list) == 1:
+            return float(rec_list[0] * 4)
+
     return "Perimeter = " + str(perimeter()) + " Area = " + str(area())
 
 
 if __name__ == '__main__':
-    print(measurements([2, 3]))
+    print(measurements([2]))
